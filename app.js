fc86fc88414daae93613411262d5d7fa72efcca8
Tyler Iguchi <tyleriguchi@gmail.com>
2020-04-04 21:31:46 -0700

    add cors

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
+
+const corsOptions = {};
 
 if (process.env.NODE_ENV !== "production") {
   // load environment variables
   require("dotenv").config();
-}
 
+  corsOptions["host"] = "*";
+} else {
+}
 var indexRouter = require("./routes/index");
 var authorizationRouter = require("./routes/authorize");
 
@@ -18,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 app.use("/authorize", authorizationRouter);
