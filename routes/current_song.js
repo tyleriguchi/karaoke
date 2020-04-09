e38855ed572582abfeab69d74fa894719ffd00b0
Tyler Iguchi <tyleriguchi@gmail.com>
2020-04-08 20:02:39 -0700

    test lyric genius api

--- routes/current_song.js
+++ routes/current_song.js
@@ -4,9 +4,29 @@ const querystring = require("querystring");
 const axios = require("axios"); // "Request" library
 
 router.get("/", function(req, res, next) {
+  // const options = {
+  //   url: "https://api.spotify.com/v1/me/player/currently-playing",
+  //   headers: { Authorization: `${req.headers.authorization}` }
+  // };
+  //
+  // // use the access token to access the Spotify Web API
+  // console.log("before user me", req.headers);
+  // axios
+  //   .request(options)
+  //   .then(response => {
+  //     console.log(response.data);
+  //     res.send({
+  //       data: response.data
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log("err", err);
+  //   });
   const options = {
-    url: "https://api.spotify.com/v1/me/player/currently-playing",
-    headers: { Authorization: `${req.headers.authorization}` }
+    url: "https://api.genius.com/search?q=Kendrick%20Lamar",
+    headers: {
+      Authorization: `Bearer ${process.env.lyric_genius_client_token}`
+    }
   };
 
   // use the access token to access the Spotify Web API
@@ -22,6 +42,7 @@ router.get("/", function(req, res, next) {
     .catch(err => {
       console.log("err", err);
     });
+  lyric_genius_client_token;
 });
 
 module.exports = router;
