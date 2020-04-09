45b863ec0b9f30604af4ecb5bb99419ebef7997f
Tyler Iguchi <tyleriguchi@gmail.com>
2020-04-08 20:44:48 -0700

    use real track

--- routes/current_song.js
+++ routes/current_song.js
@@ -5,51 +5,50 @@ const axios = require("axios"); // "Request" library
 const cheerio = require("cheerio");
 
 router.get("/", function(req, res, next) {
-  // const options = {
-  //   url: "https://api.spotify.com/v1/me/player/currently-playing",
-  //   headers: { Authorization: `${req.headers.authorization}` }
-  // };
-  //
-  // // use the access token to access the Spotify Web API
-  // console.log("before user me", req.headers);
-  // axios
-  //   .request(options)
-  //   .then(response => {
-  //     console.log(response.data);
-  //     res.send({
-  //       data: response.data
-  //     });
-  //   })
-  //   .catch(err => {
-  //     console.log("err", err);
-  //   });
-  const options = {
-    url: "https://api.genius.com/search?q=Kendrick%20Lamar",
-    headers: {
-      Authorization: `Bearer ${process.env.lyric_genius_client_token}`
-    }
-  };
-
   axios
-    .request(options)
-    .then(response => {
-      console.log(response.data.response.hits);
-      const most_likely_match = response.data.response.hits[0].result;
+    .request({
+      url: "https://api.spotify.com/v1/me/player/currently-playing",
+      headers: { Authorization: `${req.headers.authorization}` }
+    })
+    .then(spotifyResponse => {
+      console.log(spotifyResponse.data);
+
+      const trackName = spotifyResponse.data.item.name;
+      const primaryArtist = spotifyResponse.data.item.artists[0].name;
+      const params = querystring.stringify({
+        q: `${trackName} ${primaryArtist}`
+      });
+
+      const options = {
+        url: `https://api.genius.com/search?${params}`,
+        headers: {
+          Authorization: `Bearer ${process.env.lyric_genius_client_token}`
+        }
+      };
 
       axios
-        .request({ url: most_likely_match.url })
-        .then(lyric_genius_response => {
-          const $ = cheerio.load(lyric_genius_response.data);
+        .request(options)
+        .then(response => {
+          const mostLikelyMatch = response.data.response.hits[0].result;
 
-          const text = $(".lyrics").text();
-          console.log("extracted lyrics", text);
+          axios
+            .request({ url: mostLikelyMatch.url })
+            .then(lyricGeniusResponse => {
+              const $ = cheerio.load(lyricGeniusResponse.data);
 
-          res.send({
-            data: {
-              text: text,
-              url: most_likely_match.url
-            }
-          });
+              const text = $(".lyrics").text();
+              console.log("extracted lyrics", text);
+
+              res.send({
+                data: {
+                  text: text,
+                  url: mostLikelyMatch.url
+                }
+              });
+            });
+        })
+        .catch(err => {
+          console.log("err", err);
         });
     })
     .catch(err => {
